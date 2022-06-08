1146080bda34e42af741e48568e694a7b9ba5f8d
yurcifer <yurcifer.dev@gmail.com>
2022-06-08 19:53:11 +0300

    refactor in search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,35 +1,37 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import { getSearched } from '../api/search'
 import { BackToTopButton } from '../components/BackToTop/BackToTopButton';
 import { Grid } from '../components/Grid/Grid';
 
 
-export const Search = ({offset = 0, limit = 25, setOffset}) => {
+export const Search = ({offset = 0, limit = 25, setOffset, location}) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true)
   const {search} = useLocation();
 
   const params = useMemo( () => new URLSearchParams(search), [search]);
   const searchQuery = params.get('q')
+  const ref = useRef(null);
 
   const expandItems = function (data) {
     setItems([...items, ...data]);
+    if (ref.current !== searchQuery) { 
+      setItems([...data]);
+    }
+    ref.current = searchQuery;
   }
-  
+
   const fetchSearched = async (query) => {
     const {data} = await getSearched(query, limit, offset);
-    setIsLoading(false)
+    expandItems(data);
+    setIsLoading(false);
     return data;
   }
 
-  useEffect( () => {
-    fetchSearched(searchQuery).then( (data) => expandItems(data))
-  }, [offset, limit, searchQuery]);
-
-  useEffect( () => {
-    fetchSearched(searchQuery).then( (data) => setItems([...data]))
-  }, [searchQuery]);
+  useEffect(() => {
+    fetchSearched(searchQuery);
+  }, [searchQuery, offset]);
 
   return (
     <div>
